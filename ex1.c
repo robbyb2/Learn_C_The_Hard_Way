aeda7cf1237558a683e4fc9304572ac6f2d55cbf
Robby Brines <robbyb2@gmail.com>
2015-09-29 21:03:30 -0500

    Chapter1 and 2

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* This is a comment. */
+
+int main(int argc, char *argv[]){
+	int distance = 100;
+	// Try something else
+	// This is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
